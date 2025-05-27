b662695ab2298fd3edc24318d94ef57f0fc9d50b
MatheusXavier7 <matheus.gazire@sga.pucminas.br>
2025-05-27 12:16:03 -0300

    Sprint 6- Salvando Imagens no Firebase

---
+++ src/back/sobrecho/src/main/java/com/sobrecho/firebase/FirebaseInitialization.java
@@ -0,0 +1,42 @@
+package com.sobrecho.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class FirebaseInitialization {
+
+    @PostConstruct
+    public void initialization() {
+        try {
+            InputStream serviceAccount = getClass().getClassLoader()
+                    .getResourceAsStream("sobrecho-7728b-firebase-adminsdk-fbsvc-27af6b1371.json");
+
+            if (serviceAccount == null) {
+                throw new IOException("Firebase service account file not found in resources");
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setStorageBucket("sobrecho") 
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("Firebase initialized successfully");
+            } else {
+                System.out.println("Firebase already initialized");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to initialize Firebase: " + e.getMessage());
+        }
+    }
+}
--- src/back/sobrecho/src/main/java/com/sobrecho/model/User.java
+++ src/back/sobrecho/src/main/java/com/sobrecho/model/User.java
@@ -211,92 +211,17 @@ public class User {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((documentNumber == null) ? 0 : documentNumber.hashCode());
-		result = prime * result + ((documentType == null) ? 0 : documentType.hashCode());
-		result = prime * result + ((addressLine == null) ? 0 : addressLine.hashCode());
-		result = prime * result + ((country == null) ? 0 : country.hashCode());
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
-		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
-		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
-		return result;
-	}
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (documentNumber == null) {
-			if (other.documentNumber != null)
-				return false;
-		} else if (!documentNumber.equals(other.documentNumber))
-			return false;
-		if (documentType != other.documentType)
-			return false;
-		if (addressLine == null) {
-			if (other.addressLine != null)
-				return false;
-		} else if (!addressLine.equals(other.addressLine))
-			return false;
-		if (country == null) {
-			if (other.country != null)
-				return false;
-		} else if (!country.equals(other.country))
-			return false;
-		if (state == null) {
-			if (other.state != null)
-				return false;
-		} else if (!state.equals(other.state))
-			return false;
-		if (zipCode == null) {
-			if (other.zipCode != null)
-				return false;
-		} else if (!zipCode.equals(other.zipCode))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (createdAt == null) {
-			if (other.createdAt != null)
-				return false;
-		} else if (!createdAt.equals(other.createdAt))
-			return false;
-		if (updatedAt == null) {
-			if (other.updatedAt != null)
-				return false;
-		} else if (!updatedAt.equals(other.updatedAt))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     public List<Product> getProducts() {
         return products;
