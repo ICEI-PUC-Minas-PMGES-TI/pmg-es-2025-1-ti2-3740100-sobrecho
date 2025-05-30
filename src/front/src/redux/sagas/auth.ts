18b45f77761c31ae744ddfe6adb2ed2c56b9c4f5
Bernardo Meirelles <bernardomrl@icloud.com>
2025-05-30 15:13:47 -0300

    fix(auth): corrigido estrutura do sagas para enviar corretamente os dados para o cadastro de usuario

--- src/front/src/redux/sagas/auth.ts
+++ src/front/src/redux/sagas/auth.ts
@@ -5,12 +5,20 @@ import {
 	IPostAuthRegisterRequestAction,
 	IPostAuthResetPasswordRequestAction
 } from '@/redux/types';
-import { all, put, takeLatest } from 'redux-saga/effects';
+import { api } from '@/services';
+import { redirect } from 'next/navigation';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 import { toast } from 'sonner';
 
 function* postAuthLoginRequest({ email, password }: IPostAuthLoginRequestAction) {
 	try {
 		console.log('(REDUX) postAuthLoginRequest: ', email, password);
+		const { data } = yield call(api.post, 'login', {
+			email,
+			password
+		});
+		toast.success('Login realizado com sucesso!')
+		yield put(AuthCreators.postAuthLoginSuccess(data));
 	} catch (error) {
 		toast.error(String(error));
 		yield put(AuthCreators.postAuthLoginFailure());
@@ -24,6 +32,10 @@ function* postAuthRegisterRequest({
 }: IPostAuthRegisterRequestAction) {
 	try {
 		console.log('(REDUX) postAuthRegisterRequest: ', name, email, password);
+		// const { data } = yield call(api.post, 'register', { name, email, password });
+		const { data } = yield call(api.post, 'user', { name, email, password });
+		toast.success('Cadastro realizado com sucesso!');
+		yield put(AuthCreators.postAuthRegisterSuccess(data));
 	} catch (error) {
 		toast.error(String(error));
 		yield put(AuthCreators.postAuthRegisterFailure());
@@ -33,6 +45,9 @@ function* postAuthRegisterRequest({
 function* postAuthForgotPasswordRequest({ email }: IPostAuthForgotPasswordRequestAction) {
 	try {
 		console.log('(REDUX) postAuthForgotPasswordRequest: ', email);
+		yield call(api.post, 'forgot-password', { email });
+		toast.success('Email de recuperação enviado com sucesso!');
+		yield put(AuthCreators.postAuthForgotPasswordSuccess());
 	} catch (error) {
 		toast.error(String(error));
 		yield put(AuthCreators.postAuthForgotPasswordFailure());
@@ -51,6 +66,9 @@ function* postAuthResetPasswordRequest({
 			password,
 			passwordConfirm
 		);
+		yield call(api.post, 'reset-password', { token, password, passwordConfirm });
+		toast.success('Senha alterada com sucesso!');
+		yield put(AuthCreators.postAuthResetPasswordSuccess());
 	} catch (error) {
 		toast.error(String(error));
 		yield put(AuthCreators.postAuthResetPasswordFailure());
